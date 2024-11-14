6ea2d55faf32fd4cad30b9b8fe6c144996cafddb
Luuk-Nagelmaeker <ljbnagelmaeker@gmail.com>
2024-11-14 10:29:11 +0100

    initial creation of program

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -4,17 +4,25 @@ from numpy import pi
 import pandas as pd
 
 
+@click.group()
+def cmd_group():
+    pass
+
+
+@cmd_group.command()
+@click.argument('number')
 def sin(number):
-    x = np.linspace(0, 2 * pi, number)
+    x = np.linspace(0, 2 * pi, int(number))
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
+@cmd_group.command()
+@click.argument('number')
 def tan(number):
-    x = np.linspace(0, 2 * pi, number)
+    x = np.linspace(0, 2 * pi, int(number))
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
 
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
