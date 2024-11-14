4baf2fe615417ee3844ba3d838fbb6ce12eb469e
Luuk-Nagelmaeker <ljbnagelmaeker@gmail.com>
2024-11-14 10:48:24 +0100

    added help tekst and finished smallangle

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -10,16 +10,32 @@ def cmd_group():
 
 
 @cmd_group.command()
-@click.argument('number')
-def sin(number):
-    x = np.linspace(0, 2 * pi, int(number))
+@click.option(
+    "-n",
+    default=10,)
+def sin(n):
+    """calculate the sin of N numbers
+
+    the N numbers are equidistant between 0 and 2 pi
+
+    default = 10
+    """
+    x = np.linspace(0, 2 * pi, int(n))
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
 @cmd_group.command()
-@click.argument('number')
-def tan(number):
-    x = np.linspace(0, 2 * pi, int(number))
+@click.option(
+    "-n",
+    default=10,)
+def tan(n):
+    """calculate the tan of N numbers
+
+    the N numbers are equidistant between 0 and 2 pi
+
+    default = 10
+    """
+    x = np.linspace(0, 2 * pi, int(n))
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
